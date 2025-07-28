63ec536f57dafe599548b426da57267fe1e07775
toaster-vip <toaster.vip@gmail.com>
2025-07-28 11:41:10 -0400

    更新 notifier.py

--- notifier.py
+++ notifier.py
@@ -2,6 +2,7 @@ import requests
 from config import CONFIG
 
 def send_notification(message):
+    print("[DEBUG] 我从 iPad 修改了代码！")
     key = CONFIG.get("SERVER_CHAN_KEY")
     if key:
         requests.get(f"https://sctapi.ftqq.com/{key}.send", params={"title":"Trade Notification", "desp": message})
