2dd20fdf9525bacab3e49457e9a903ac094062d4
Sufeng Ding <toaster.vip@gmail.com>
2025-07-28 15:35:36 -0400

    update

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 CONFIG = {
-    "API_KEY": "WpWVkahrWSCaJfcmvcJgSv",
-    "API_SECRET": "cxakp_FDRiZ8aw9UPogTVTgzzJGv",
+    "API_KEY": "YVzxNDmnAR7so8EjrW33yK",
+    "API_SECRET": "cxakp_EcuTu92ZQbMxmmqNKhBDj6",
     "BASE_URL": "https://api.crypto.com/v2",
     "SIMULATE": False,  # ⛔ 真实交易模式
     "SERVER_CHAN_KEY": "SCT290772THBFAsWEtLa29M3l98qRSZ1DZ",
