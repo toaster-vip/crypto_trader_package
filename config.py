ee5d3e1a5ad2a4420069311d6420aa8fc351bf4f
toaster-vip <toaster.vip@gmail.com>
2025-07-30 21:59:09 -0400

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ CONFIG = {
     "KUCOIN_API_SECRET": "473367a6-af01-48d2-8b78-2817ab879dc1",  # ✅ 新 Secret
     "KUCOIN_API_PASSPHRASE": "ilovesophia",
 
-    "SIMULATE": False,  # ✅ 模拟交易模式
+    "SIMULATE": True,  # ✅ 模拟交易模式
     "SERVER_CHAN_KEY": "SCT290772THBFAsWEtLa29M3l98qRSZ1DZ",
 
     "TRADE": {
