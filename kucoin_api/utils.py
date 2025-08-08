473608c5cf598bc0edbdadcb6bb527f1b759150b
toaster-vip <toaster.vip@gmail.com>
2025-08-08 09:52:39 -0400

    Update utils.py

--- kucoin_api/utils.py
+++ kucoin_api/utils.py
@@ -1,1 +1,13 @@
-a
+def safe_float(x, default=0.0):
+    try:
+        return float(x)
+    except (TypeError, ValueError):
+        return default
+
+def to_symbol_pair(symbol):
+    s = symbol.upper()
+    if "-" in s:
+        return s
+    if not s.endswith("USDT"):
+        return f"{s}-USDT"
+    return s
